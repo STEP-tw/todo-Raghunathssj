f7298877dfaa9547b3a1a21e56144596cedfbb95
raghunathssj <ssjrnath1@gmail.com>
2018-01-12 13:35:35 +0530

    written test for file not found. 	-included package.json. 	-seperated server from app

---
+++ app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const timeStamp = require('./time.js').timeStamp;
+const WebApp = require('./webapp');
+
+let toS = o=>JSON.stringify(o,null,2);
+
+
+let logRequest = (req,res)=>{
+  let text = ['------------------------------',
+    `${timeStamp()}`,
+    `${req.method} ${req.url}`,
+    `HEADERS=> ${toS(req.headers)}`,
+    `COOKIES=> ${toS(req.cookies)}`,
+    `BODY=> ${toS(req.body)}`,''].join('\n');
+  fs.appendFile('request.log',text,()=>{});
+
+  console.log(`${req.method} ${req.url}`);
+}
+
+let app = WebApp.create();
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,25 +1,5 @@
-const fs = require('fs');
 const http = require('http');
-const WebApp = require('./webapp');
-const timeStamp = require('./time.js').timeStamp;
-
-let toS = o=>JSON.stringify(o,null,2);
-
-
-let logRequest = (req,res)=>{
-  let text = ['------------------------------',
-    `${timeStamp()}`,
-    `${req.method} ${req.url}`,
-    `HEADERS=> ${toS(req.headers)}`,
-    `COOKIES=> ${toS(req.cookies)}`,
-    `BODY=> ${toS(req.body)}`,''].join('\n');
-  fs.appendFile('request.log',text,()=>{});
-
-  console.log(`${req.method} ${req.url}`);
-}
-
-let app = WebApp.create();
-
+const app = require('./app.js');
 const PORT = 8080;
 let server = http.createServer(app);
 server.on('error',e=>console.error('**error**',e.message));
