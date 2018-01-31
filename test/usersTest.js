eb3c42f161c59aaf8759bf416c275780cf250e9c
arvind singh <1141191arvind.singh@gamil.com>
2018-01-31 19:02:02 +0530

    fixed a bug

--- test/usersTest.js
+++ test/usersTest.js
@@ -10,11 +10,11 @@ describe('Users',() => {
   describe('getUser',() => {
     it('should get the user if the username and password is matches with any of the users in registered users',() => {
       const expected= {
-        username : 'tester',
-        name: 'Tester',
-        password : 'test'
+        username : 'admin',
+        name: 'Admin',
+        password : 'password'
       };
-      const actual = users.getUser('tester','test');
+      const actual = users.getUser('admin','password');
       assert.deepEqual(actual,expected);
     });
     it("should return undefined if the username and password doesn't matches with any of the users in registered users",() => {
@@ -23,8 +23,8 @@ describe('Users',() => {
   });
   describe('getUserData',() => {
     it('should return userdata if the given username matches with any of the users data in database',() => {
-      const expected = new User('tester','Tester');
-      const actual = users.getUserData('tester');
+      const expected = new User('admin','Admin');
+      const actual = users.getUserData('admin');
       assert.deepEqual(actual,expected);
     });
     it("should return undefined if the username doesn't matches with any of the users data in database",() => {
@@ -33,10 +33,10 @@ describe('Users',() => {
   });
   describe('getUserBySessionId',() => {
     it('should return user data if the session id matches with any of the users session id',() => {
-      const user = users.getUser('tester','test');
+      const user = users.getUser('admin','password');
       user.sessionid = 12345;
       const actual = users.getUserBySessionId(12345);
-      const expected = new User('tester','Tester');
+      const expected = new User('admin','Admin');
       assert.deepEqual(actual,expected);
     });
     it("should return undefined if the session id doesn't matches with any of the users session id in registered users",() => {
@@ -45,7 +45,7 @@ describe('Users',() => {
   });
   describe('#saveData', () => {
     it('should save data in database', () => {
-      assert.isUndefined(users.saveData());  
+      assert.isUndefined(users.saveData());
     });
   });
 });
